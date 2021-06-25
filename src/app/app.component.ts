1dc2a2dafe930e5079b1f7513f5de5480e2431e5
Nazeel <nazeelash@gmail.com>
2021-06-25 20:07:33 +0530

    modified component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,60 +10,29 @@ export class AppComponent {
   myForm: FormGroup;
   public availabelOptions: any[];
   public selectedOptions: any[];
-  options:any[] =[{
-    value:'0-24',
-    checked:false
-  },{
-    value:'25-99',
-    checked:false
-  },{
-    value:'99-249',
-    checked:false
-  },{
-    value:'250-499',
-    checked:false
-  },{
-    value:'500-999',
-    checked:false
-  },{
-    value:'1000-4999',
-    checked:false
-  },{
-    value:'5000-9999',
-    checked:false
-  },{
-    value:'10000-49999',
-    checked:false
-  },{
-    value:'50000-100000',
-    checked:false
-  },{
-    value:'>100000',
-    checked:false
-  }];
+  options:any[] =[
+    '0-24',
+    '25-99',
+    '99-249',
+    '250-499',
+    '500-999',
+    '1000-4999',
+    '5000-9999',
+    '10000-49999',
+    '50000-100000',
+    '>100000',
+  ];
   constructor(
     private formBuilder: FormBuilder
   ) {
-    //this.availabelOptions = this.options;
     this.createContactForm();
   }
   createContactForm(){
     this.myForm = this.formBuilder.group({
-      options: [this.options, Validators.required],
+      options: [this.options.slice(0, 5), Validators.required],
     });
   }
   get formData() {
     return this.myForm.controls;
-  }
-  push(){
-   this.availabelOptions = this.selectedOptions;
-   this.availabelOptions.map(item=>item.checked=false);
-   this.selectedOptions = []
-  }
-  selectAll(){
-
-  }
-  clearAll(){
-    
   }
 }
--- src/components/multi-select-options/multi-select-options.component.ts
+++ src/components/multi-select-options/multi-select-options.component.ts
@@ -1,30 +1,94 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Optional, Output, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-multi-select-options',
   templateUrl: './multi-select-options.component.html',
   styleUrls: ['./multi-select-options.component.css']
 })
-export class MultiSelectOptionsComponent implements OnInit {
+export class MultiSelectOptionsComponent implements OnInit, ControlValueAccessor {
   @Input() PData: any[];
-  @Output() childEvent = new EventEmitter();
-  temporaryData :any[] = [];
-  constructor() { }
+  @Input() disabled: boolean;
+  selectedData: any[] = [];
+
+  constructor(
+    @Self()
+    @Optional()
+    private ngControl: NgControl
+  ) {
+    if(this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
 
   ngOnInit(): void {
+    this.PData = this.PData.map(item => ({ value: item, checked: false}));
+    const selectedValues = this.selectedData.map(item => item.value);
+    this.PData = this.selectedData ? this.PData.filter(item => !selectedValues.includes(item.value)) : this.PData;
   }
 
-  select(index:any){
-    this.PData[index].checked = ! this.PData[index].checked;
-    if(this.PData[index].checked){
-      this.temporaryData.push(this.PData[index]);
-    }else{
-      this.temporaryData = this.temporaryData.filter(item => item.checked == true);
-    }
-    
-    //this.PData.map((item)=> item.checked ? this.temporaryData.push(item):'');
-    console.log(this.temporaryData);
-    this.childEvent.emit(this.temporaryData);
+  select(item:any){
+    this.onTouched();
+    item.checked = !item.checked;
   }
+
+  push(){
+    const toBeUnselected = this.selectedData
+      .filter(item => item.checked)
+      .map(item => ({...item, checked: false}));
+
+    const toBeSelected = this.PData
+      .filter(item => item.checked)
+      .map(item => ({...item, checked: false}));
+
+    const unselectedRight = this.selectedData
+      .filter(item => !item.checked);
+
+    const unselectedLeft = this.PData
+      .filter(item => !item.checked);
+
+    this.selectedData = [
+      ...unselectedRight, 
+      ...toBeSelected
+    ];
+
+    this.PData = [
+      ...unselectedLeft,
+      ...toBeUnselected
+    ];
+
+    this.onChange(this.selectedData.map(item => item.value));
+  }
+
+  selectAll(){
+    this.selectedData = [...this.selectedData, ...this.PData];
+    this.PData = [];
+    this.onChange(this.selectedData.map(item => item.value));
+  }
+
+  clearAll(){
+    this.PData = [...this.PData, ...this.selectedData];
+    this.selectedData = [];
+    this.onChange([]);
+  }
+
+  writeValue(obj: any): void {
+    this.selectedData = obj ? obj.map((item: any) => ({ value: item, checked: false})) : [];
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+
+  onChange(val: any) {}
+  onTouched() {}
 
 }
